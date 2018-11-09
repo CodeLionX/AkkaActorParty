215111868a978408981e29dbe7bd63c20bd11677
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-09 21:44:41 +0100

    remove abbr. and make commands runnable

--- src/main/scala/com/github/leananeuber/hasher/cli/StartMasterCommand.scala
+++ src/main/scala/com/github/leananeuber/hasher/cli/StartMasterCommand.scala
@@ -3,9 +3,15 @@ package com.github.leananeuber.hasher.cli
 import org.backuity.clist
 
 
-class StartMasterCommand extends clist.Command(
+object StartMasterCommand extends clist.Command(
     name = "master",
     description = "start a master actor system") with CommonStartCommand {
 
+  val masterRole = "master"
+
   override def defaultPort: Int = CommonStartCommand.defaultMasterPort
+
+  override def run(actorSystemName: String): Unit = {
+
+  }
 }
--- src/main/scala/com/github/leananeuber/hasher/cli/StartSlaveCommand.scala
+++ src/main/scala/com/github/leananeuber/hasher/cli/StartSlaveCommand.scala
@@ -3,23 +3,27 @@ package com.github.leananeuber.hasher.cli
 import org.backuity.clist
 
 
-class StartSlaveCommand extends clist.Command(
+object StartSlaveCommand extends clist.Command(
   name = "slave",
   description = "start a slave actor system") with CommonStartCommand {
 
+  val slaveRole = "slave"
+
   var masterHost: String = clist.opt[String](
     name = "masterhost",
-    abbrev = "mh",
     description = "host name or IP of the master",
     default = defaultHost
   )
 
   var masterPort: Int = clist.opt[Int](
     name = "masterport",
-    abbrev = "mp",
     description = "port to bind against",
     default = CommonStartCommand.defaultMasterPort
   )
 
   override def defaultPort: Int = CommonStartCommand.defaultSlavePort
+
+  override def run(actorSystemName: String): Unit = {
+
+  }
 }
