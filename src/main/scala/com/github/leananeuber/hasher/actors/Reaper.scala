b399fdc9732cc867b1afee4f3837983446c46a08
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-13 20:15:49 +0100

    refactor reaper and add register method

--- src/main/scala/com/github/leananeuber/hasher/actors/Reaper.scala
+++ src/main/scala/com/github/leananeuber/hasher/actors/Reaper.scala
@@ -1,13 +1,24 @@
-package com.github.leananeuber.hasher
+package com.github.leananeuber.hasher.actors
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
+import akka.actor.{Actor, ActorContext, ActorLogging, ActorRef, Props, Terminated}
+
 import scala.collection.mutable.ArrayBuffer
 
 //reaper-companion
 //reaper-messages
 object Reaper {
+
+  val reaperName = "reaper"
+
   def props: Props = Props[Reaper]
+
+  def watchWithDefault(actor: ActorRef)(implicit context: ActorContext): Unit = {
+    val reaper = context.system.actorSelection(s"/user/$reaperName")
+    reaper ! WatchMe(actor)
+  }
+
   case class WatchMe(ref: ActorRef)
+
 }
 
 
@@ -15,20 +26,22 @@ object Reaper {
 class Reaper extends Actor with ActorLogging{
   import Reaper._
 
-  val watched = ArrayBuffer.empty[ActorRef]
+  val watched: ArrayBuffer[ActorRef] = ArrayBuffer.empty[ActorRef]
 
   override def preStart(): Unit = {
     super.preStart()
     log.info("Started {}", this)
   }
 
-  def receive = {
+  def receive: Receive = {
     case WatchMe(ref) =>
       context.watch(ref)
       watched+=ref
+
     case Terminated(ref) =>
       watched-=ref
       if(watched.isEmpty) terminateSystem()
+
     case unexpected =>
       log.error(s"ERROR: Unknown message: $unexpected")
   }
