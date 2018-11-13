b7565e65eab8a01820dea57f252c418af7b53590
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-14 00:35:06 +0100

    more logging in worker and we now use only a pre-defined password-range

--- src/main/scala/com/github/leananeuber/hasher/actors/password_cracking/PasswordCrackingWorker.scala
+++ src/main/scala/com/github/leananeuber/hasher/actors/password_cracking/PasswordCrackingWorker.scala
@@ -6,7 +6,7 @@ import java.security.MessageDigest
 import akka.actor.{Actor, ActorLogging, ActorRef, Cancellable, Props}
 import com.github.leananeuber.hasher.MasterWorkerProtocol.{RegisterWorker, RegisterWorkerAck}
 import com.github.leananeuber.hasher.actors.Reaper
-import com.github.leananeuber.hasher.actors.password_cracking.PasswordCrackingWorker.CrackingFailedException
+import com.github.leananeuber.hasher.actors.password_cracking.PasswordCrackingProtocol.{CrackPasswordsCommand, PasswordsCrackedEvent}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -15,16 +15,18 @@ import scala.language.postfixOps
 
 object PasswordCrackingWorker {
 
+  val passwordRange: Range = 0 to 1000000
+
   def props(master: ActorRef): Props =
     Props(new PasswordCrackingWorker(master))
 
-  object CrackingFailedException extends RuntimeException
+  case class CrackingFailedException(m: String) extends RuntimeException(m)
 
 }
 
 
-class PasswordCrackingWorker(master: ActorRef)
-  extends Actor with ActorLogging {
+class PasswordCrackingWorker(master: ActorRef) extends Actor with ActorLogging {
+  import PasswordCrackingWorker._
 
   val name: String = self.path.name
   val registerWorkerCancellable: Cancellable =
@@ -44,18 +46,23 @@ class PasswordCrackingWorker(master: ActorRef)
       log.info(s"$name: successfully registered at master actor")
 
     case CrackPasswordsCommand(secrets) =>
+      log.info(s"$name: working on ${secrets.keys} passwords")
       sender() ! PasswordsCrackedEvent(decrypt(secrets))
+
+    // catch-all case: just log
+    case m =>
+      log.warning(s"$name: Received unknown message: $m")
   }
 
   def decrypt(secrets: Map[Int, String]): Map[Int, Int] = secrets.map( idHashTuple => {
     val realValue = unhash(idHashTuple._2) match {
       case Some(real) => real
-      case None => throw CrackingFailedException
+      case None => throw CrackingFailedException("Could not decrypt password hash")
     }
     idHashTuple._1 -> realValue
   })
 
-  private def unhash(hexHash: String): Option[Int] = (0 to hexHash.length)
+  private def unhash(hexHash: String): Option[Int] = passwordRange
     .map ( i => i -> hash(i) )
     .find{ case (_, h) => h.equals(hexHash) }
     .map ( indexHashTuple => indexHashTuple._1 )
