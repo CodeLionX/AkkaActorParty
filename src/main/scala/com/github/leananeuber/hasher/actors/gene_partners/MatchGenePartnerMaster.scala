8bedd68d8522492eb53a5e84faaa8aac27498cc4
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-19 15:51:33 +0100

    work on gene master; wip

--- src/main/scala/com/github/leananeuber/hasher/actors/gene_partners/MatchGenePartnerMaster.scala
+++ src/main/scala/com/github/leananeuber/hasher/actors/gene_partners/MatchGenePartnerMaster.scala
@@ -1,12 +1,14 @@
 package com.github.leananeuber.hasher.actors.gene_partners
 
-import akka.actor.{Actor, ActorLogging, ActorRef, PoisonPill, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, PoisonPill, Props, Terminated}
 import com.github.leananeuber.hasher.actors.Reaper
 import com.github.leananeuber.hasher.actors.gene_partners.MatchGenePartnerMaster.StartMatching
 import com.github.leananeuber.hasher.actors.gene_partners.MatchGenePartnerWorker.CalculateLCSLengths
 import com.github.leananeuber.hasher.protocols.MasterWorkerProtocol.{RegisterWorker, RegisterWorkerAck}
 
 import scala.collection.mutable
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 
 object MatchGenePartnerMaster {
@@ -35,25 +37,30 @@ class MatchGenePartnerMaster(nWorkers: Int) extends Actor with ActorLogging {
     workers.foreach(_ ! PoisonPill)
   }
 
-  override def receive: Receive = waitForWorkers()
+  override def receive: Receive = {
 
-  def waitForWorkers(): Receive = {
     case RegisterWorker =>
       workers.add(sender)
       context.watch(sender)
       sender ! RegisterWorkerAck
       log.info(s"$name: worker $sender registered")
 
-      if(workers.size == nWorkers) context.become(ready)
-  }
-
-  def ready: Receive = {
     case StartMatching(genes) =>
-      val combinations = ???
-      val ranges: Seq[Seq[(Int, Int)]] = ???
+      if(workers.size < nWorkers) {
+        // delay processing of message until all workers are ready
+        context.system.scheduler.scheduleOnce(1 second, self, StartMatching(genes))
 
-      workers.zip(ranges).foreach{ case (ref, indexRange) =>
-        ref ! CalculateLCSLengths(genes, indexRange)
+      } else {
+        val combinations = ???
+        val ranges: Seq[Seq[(Int, Int)]] = ???
+
+        workers.zip(ranges).foreach { case (ref, indexRange) =>
+          ref ! CalculateLCSLengths(genes, indexRange)
+        }
       }
+
+    case Terminated(actorRef) =>
+      workers.remove(actorRef)
+      log.warning(s"$name: worker $actorRef terminated - was it on purpose?")
   }
 }
