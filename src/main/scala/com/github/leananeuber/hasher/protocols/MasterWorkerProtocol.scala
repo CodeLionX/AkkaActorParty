67fa3f9198731b0c00a2bc825c857dc4f59abdcd
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-19 15:51:54 +0100

    extract master worker communication handler

--- src/main/scala/com/github/leananeuber/hasher/protocols/MasterWorkerProtocol.scala
+++ src/main/scala/com/github/leananeuber/hasher/protocols/MasterWorkerProtocol.scala
@@ -1,8 +1,30 @@
 package com.github.leananeuber.hasher.protocols
 
+import akka.actor.{Actor, ActorLogging, ActorRef, Terminated}
+
+import scala.collection.mutable
+
 object MasterWorkerProtocol {
 
   case object RegisterWorker extends SerializableMessage
   case object RegisterWorkerAck extends SerializableMessage
+
+
+  trait MasterHandling { self: Actor with ActorLogging =>
+
+    val workers: mutable.Set[ActorRef] = mutable.Set.empty
+
+    override def receive: Receive = {
+      case RegisterWorker =>
+        workers.add(sender)
+        context.watch(sender)
+        sender ! RegisterWorkerAck
+        log.info(s"worker $sender registered")
+
+      case Terminated(actorRef) =>
+        workers.remove(actorRef)
+        log.warning(s"worker $actorRef terminated - was it on purpose?")
+    }
+  }
 
 }
