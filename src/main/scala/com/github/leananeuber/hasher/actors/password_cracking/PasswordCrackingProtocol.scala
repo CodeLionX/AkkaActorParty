a18944b4f8ef6e02e4183ca1c7daae3eac6b13c3
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-23 21:10:50 +0100

    fix message hierarchy for correct serialization

--- src/main/scala/com/github/leananeuber/hasher/actors/password_cracking/PasswordCrackingProtocol.scala
+++ src/main/scala/com/github/leananeuber/hasher/actors/password_cracking/PasswordCrackingProtocol.scala
@@ -1,11 +1,13 @@
 package com.github.leananeuber.hasher.actors.password_cracking
 
+import com.github.leananeuber.hasher.protocols.SerializableMessage
+
 object PasswordCrackingProtocol {
 
-  case class StartCrackingCommand(secrets: Map[Int, String])
+  case class StartCrackingCommand(secrets: Map[Int, String]) extends SerializableMessage
 
-  case class CrackPasswordsCommand(secrets: Map[Int, String], hashRange: Seq[Int])
+  case class CrackPasswordsCommand(secrets: Map[Int, String], hashRange: Seq[Int]) extends SerializableMessage
 
-  case class PasswordsCrackedEvent(cleartexts: Map[Int, Int])
+  case class PasswordsCrackedEvent(cleartexts: Map[Int, Int]) extends SerializableMessage
 
 }
