7e0907940cc95072e4f33b63192781634be2b519
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-23 21:24:26 +0100

    improvements

--- src/main/scala/com/github/leananeuber/hasher/SATest.scala
+++ src/main/scala/com/github/leananeuber/hasher/SATest.scala
@@ -7,8 +7,9 @@ import scala.io.Source
 object SATest extends App{
 
   val lines = Source.fromFile("src/main/resources/students.csv").getLines().drop(1).toArray
-  println(s"Read file with ${lines.length} lines")
+  //println(s"Read file with ${lines.length} lines")
 
   val analyzer = new SerialAnalyzer
   analyzer.analyze(lines)
+
 }
