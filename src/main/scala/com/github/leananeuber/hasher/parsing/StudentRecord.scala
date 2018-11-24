59e4623d05136119cc5c3fd80d8ffd257b607b01
CodeLionX <sebastian.schmidl@t-online.de>
2018-11-24 12:42:14 +0100

    added output class definitin

--- src/main/scala/com/github/leananeuber/hasher/parsing/StudentRecord.scala
+++ src/main/scala/com/github/leananeuber/hasher/parsing/StudentRecord.scala
@@ -1,3 +1,9 @@
 package com.github.leananeuber.hasher.parsing
 
-sealed case class StudentRecord(id: Int, name: String, passwordHash: String, geneSeq: String)
+sealed case class StudentRecord(id: Int, name: String, passwordHash: String, geneSeq: String) {
+
+  def extractToResult: ResultRecord = ResultRecord(id, name, "", 0, 0, "")
+
+}
+
+sealed case class ResultRecord(id: Int, name: String, password: Int, prefix: Int, partnerId: Int, hash: String)
